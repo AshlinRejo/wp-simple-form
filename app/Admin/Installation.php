dbf24cb2de0c2a5367fdca9c4ef41ec3ce5090fc
ashlin <ashlin@flycart.org>
2023-11-23 11:22:42 +0530

    - Add table while activate the plugin if not exists.

--- app/Admin/Installation.php
+++ app/Admin/Installation.php
@@ -32,5 +32,22 @@ class Installation {
 	 * Add tables if required
 	 * */
 	private function add_db_tables() {
+		global $wpdb;
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		$wpdb->hide_errors();
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name      = $wpdb->prefix . 'wpsf_feedback';
+		$query           = "CREATE TABLE $table_name (
+                                  `id` bigint NOT NULL AUTO_INCREMENT,
+                                  `name` varchar(255) NOT NULL,
+                                  `feedback` text NOT NULL,
+                                  `created_at` timestamp NOT NULL,
+                                 PRIMARY KEY (`id`)
+                            ) $charset_collate;";
+
+        // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		if ( strtolower( $wpdb->get_var( 'show tables like ' . esc_sql( $table_name ) ) ) !== strtolower( $table_name ) ) {
+			dbDelta( $query );
+		}
 	}
 }
